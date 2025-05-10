91b6a4e0b2aac1c7031bfb632242a8d0983429c5
Pryta Rosela <132034419+prytarosela@users.noreply.github.com>
2025-05-10 20:16:59 +0700

    Update Buku.ts

--- app/Models/Buku.ts
+++ app/Models/Buku.ts
@@ -1,25 +1,25 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon' // Mengimpor kelas DateTime dari pustaka Luxon untuk menangani tanggal dan waktu
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm' // Mengimpor BaseModel dan decorator column dari ORM bawaan AdonisJS
 
 export default class Buku extends BaseModel {
-  @column({ isPrimary: true })
+  @column({ isPrimary: true }) // Menandai kolom 'id' sebagai primary key
   public id: number
 
-  @column()
+  @column() // Kolom untuk menyimpan judul buku
   public judul: string
 
-  @column()
+  @column() // Kolom untuk menyimpan nama penulis buku
   public penulis: string
 
-  @column()
+  @column() // Kolom untuk menyimpan tahun terbit dalam bentuk angka
   public tahun_terbit: number
 
-  @column()
+  @column() // Kolom deskripsi buku, bersifat opsional (nullable)
   public deskripsi: string | null
 
-  @column.dateTime({ autoCreate: true })
+  @column.dateTime({ autoCreate: true }) // Kolom otomatis terisi saat entri dibuat
   public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  @column.dateTime({ autoCreate: true, autoUpdate: true }) // Kolom otomatis terisi saat entri dibuat dan diperbarui
   public updatedAt: DateTime
 }
